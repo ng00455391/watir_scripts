ad20a28ad800b8f0d50bb475cede2cc66869e42d
Nagaraj Ganachari <nagarajsrb@gmail.com>
2016-04-07 10:16:19 +0530

    simple html page

--- homepage.rb
+++ homepage.rb
@@ -1,7 +1,10 @@
 require 'watir-webdriver'
+require 'headless'
 
 class Homepage
     def initialize
+        headless = Headless.new
+        headless.start
         @browser = Watir::Browser.new
         @browser.goto 'file:///home/nagaraj/watir/welcome.html'
     end
@@ -10,9 +13,22 @@ class Homepage
         new
     end
  
-    def welcome
-        @browser.h1(id: 'welcome')
+    def body
+        @browser.body
     end
+    def nav_bar
+        @browser.ul(class: 'nav')
+    end
+    def h1
+        @browser.h1
+    end
+    def h2
+        @browser.h2
+    end
+    def list
+        @browser.ul
+    end
+
  
     def close
         @browser.close
--- watir_test.rb
+++ watir_test.rb
@@ -1,10 +1,18 @@
+#! /home/nagaraj/.rbenv/shims/ruby
 require './assertions.rb'
 require './homepage.rb'
  
 homepage = Homepage.visit
  
-assert('that the welcome message exists') { homepage.welcome.exists? }
-assert('that the welcome message is visible') { homepage.welcome.visible? }
-assert('that the welcome message text is correct') { homepage.welcome.text == 'Welcome DevOps!!!' }
+assert(' \'the index has Body Tag included\'') { homepage.body.exists? }
+assert(' \'the index page has navigation bar\'') { homepage.nav_bar.exists? }
+assert(' \'the page has h1\'') { homepage.h1.exists? }
+assert(' \'the page has h2\'') { homepage.h2.exists? }
+assert(' \'the page has List\'') { homepage.list.exists? }
+
+# assert('that the welcome message is visible') { homepage.index.visible? }
+# assert('that the welcome message text is correct') { homepage.welcome.text == 'Home' }
+# assert('that the ') {}
+
  
 homepage.close
